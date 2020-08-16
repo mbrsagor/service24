709748f50d82de7d21a3a766e09f9777a06cfe5f
Sagor <mbrsagor@gmail.com>
2020-08-17 00:47:40 +0600

    category admin panel added fields

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from core.models.category import Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'parent', 'order_by', 'created_at', 'updated_at']
+    list_display_links = ['name']
+    list_editable = ['order_by']
+    search_fields = ['name', 'order_by']
+    list_filter = ['name', 'parent', 'order_by']
+    list_per_page = 8
+
+
+admin.site.register(Category, CategoryAdmin)
