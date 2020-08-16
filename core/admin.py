d6a2739c2734a871281b5f43a628fde53d89f97a
Sagor <mbrsagor@gmail.com>
2020-08-17 00:59:21 +0600

    Location models added and admin panel added

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from core.models.category import Category
+from core.models.location import Location
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -12,3 +13,14 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Category, CategoryAdmin)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'parent', 'created_at', 'updated_at']
+    list_display_links = ['name']
+    search_fields = ['name', 'is_active']
+    list_filter = ['name', 'parent', 'is_active']
+    list_per_page = 8
+
+
+admin.site.register(Location, LocationAdmin)
