231038417ea1e5606345de8d82cc54ab0ac24930
Sagor <mbrsagor@gmail.com>
2020-08-17 00:06:41 +0600

    Core app url import

--- service24/urls.py
+++ service24/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('service.urls')),
+    path('core/', include('core.urls')),
 ]
 
 if settings.DEBUG:
