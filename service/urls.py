66e39b67f82ac441e557ca3e0c447ca1040bbcdf
Sagor <mbrsagor@gmail.com>
2020-08-17 11:41:39 +0600

    Template seperate and diffrent directory

--- service/urls.py
+++ service/urls.py
@@ -1,1 +1,1 @@
-from django.urls import pathurlpatterns = []
+from django.urls import pathfrom .views import Dashboardurlpatterns = [    path('dashboard/', Dashboard.as_view(), name='dashboard'),]
--- service/views.py
+++ service/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class Dashboard(View):
+    def get(self, request):
+        template_name = 'dashboard.html'
+        return render(request, template_name)
