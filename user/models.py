fe41020af515e15ccc95f08b8ea572299928b898
Sagor <mbrsagor@gmail.com>
2020-08-19 01:52:02 +0600

    Base Abstract User information change

--- user/models.py
+++ user/models.py
@@ -4,10 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     _id = models.ObjectIdField()
-    email = models.EmailField(blank=True, unique=False)
-    parent = models.ForeignKey('self', on_delete=models.PROTECT, default=None, null=True, blank=True)
-    active = models.BooleanField(default=True)
-    verification_code = models.CharField(max_length=255, default=None, null=True, blank=True)
+    phone_number = models.CharField(max_length=14)
 
     def __str__(self):
-        return self.email
+        return self.phone_number
