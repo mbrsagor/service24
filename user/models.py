bf9855a0785f6d18fe43c6db12ad46293eb9017d
Sagor <mbrsagor@gmail.com>
2020-08-25 12:13:10 +0600

    User phone number unique and login page remove default forgot password link

--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     _id = models.ObjectIdField()
-    phone_number = models.CharField(max_length=14)
+    phone_number = models.CharField(max_length=14, unique=True)
 
     def __str__(self):
         return self.phone_number
