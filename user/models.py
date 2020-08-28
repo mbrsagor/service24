d4011eaa754c0fe69ec7a5498ea84fa072c14022
Sagor <mbrsagor@gmail.com>
2020-08-29 01:56:37 +0600

    User profile done

--- user/models.py
+++ user/models.py
@@ -1,9 +1,5 @@
 from djongo import models
 from django.contrib.auth.models import AbstractUser
-from django.conf import settings
-from rest_framework.authtoken.models import Token
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class User(AbstractUser):
@@ -13,9 +9,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.phone_number
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
