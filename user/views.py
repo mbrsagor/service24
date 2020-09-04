46100240f383ae4d950244dcc75397f308fe1c8f
Sagor <mbrsagor@gmail.com>
2020-09-05 01:10:26 +0600

    Agent profile bug fixed

--- user/views.py
+++ user/views.py
@@ -3,9 +3,8 @@ from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, render
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic.base import TemplateView
 
 from .models import User, Agent
 from .forms import CreateAgentFrom
@@ -58,7 +57,6 @@ class UserDeleteView(View):
 class UpdateAgentProfile(SuccessMessageMixin, UpdateView):
     success_message = "Agent successfully updated!"
     template_name = 'agent/update_agent.html'
-    success_url = f'/user/update-agent-profile/{id}'
     model = Agent
     form_class = CreateAgentFrom
 
@@ -69,9 +67,11 @@ class UpdateAgentProfile(SuccessMessageMixin, UpdateView):
     #     return get_object_or_404(Agent, user__username__iexact=username)
 
 
-class AgentProfile(TemplateView):
-    template_name = 'agent/agent_profile.html'
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
+class AgentProfile(View):
 
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(AgentProfile, self).dispatch(*args, **kwargs)
+    def get(self, request):
+        agent = Agent.objects.get(id=self.request.user.id)
+        template_name = 'agent/agent_profile.html'
+        context = {'agent': agent}
+        return render(request, template_name, context)
