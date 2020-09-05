d02d2cf299747e7f5f6599d670317f7b634cf4dc
Sagor <mbrsagor@gmail.com>
2020-09-05 12:49:33 +0600

    Service default Django admin panel added

--- service/admin.py
+++ service/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from service.models.service import Service
 
-admin.site.register(Service)
+
+class ModelService(admin.ModelAdmin):
+    list_display = ['owner', 'service_name', 'package_name', 'category', 'location', 'price', 'discount_price', \
+                    'quick_delivery_charge', 'created_at']
+    search_fields = ['service_name', 'package_name', 'price']
+    list_per_page = 8
+
+
+admin.site.register(Service, ModelService)
