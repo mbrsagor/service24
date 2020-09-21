4e0538ed1a480e7293535e0c1a16b2361103e8b8
Sagor <mbrsagor@gmail.com>
2020-09-21 23:29:26 +0600

    Order backend added two field

--- service/models/order.py
+++ service/models/order.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-from service.utils import OrderStatus
 from user.models import User
 from core.models.base import BaseEntity
+from core.models.location import Location
 from service.models.service import Service
 
 
@@ -19,9 +19,11 @@ class Order(BaseEntity):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='service_order')
     item = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='OrderItem')
     schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE, related_name='order_schedule')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='order_location')
     order_date = models.DateField()
     phone_number = models.IntegerField(default=0)
-    status = models.IntegerField(choices=OrderStatus.get_choices(), default=OrderStatus.PENDING.value)
+    status = models.BooleanField(default=False, blank=True)
+    address = models.TextField(default=None)
 
     def __str__(self):
         return self.user.username
