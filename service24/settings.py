bf2514f0c6ba688042e9ada38b9a85fc88092419
Sagor <mbrsagor@gmail.com>
2020-10-08 21:30:13 +0600

    Token authentication globally added

--- service24/settings.py
+++ service24/settings.py
@@ -142,7 +142,11 @@ DEFAULT_FROM_EMAIL = 'your email'
 # Django REST framework
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 8
+    'PAGE_SIZE': 8,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+    ]
 }
 
 CORS_ORIGIN_ALLOW_ALL = False
