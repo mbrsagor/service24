a0ad0b7056cee06f0d25ed167ad24547dcbff95e
Sagor <mbrsagor@gmail.com>
2020-10-18 11:22:10 +0600

    Payment udpateview added

--- service/models/payment.py
+++ service/models/payment.py
@@ -4,7 +4,7 @@ from core.models.base import BaseEntity
 
 
 class Payment(BaseEntity):
-    name = models.CharField(max_length=90)
+    name = models.CharField(max_length=90, unique=True)
     logo = models.ImageField(upload_to='payment/%y/%m/%d', blank=True, null=True)
 
     def __str__(self):
