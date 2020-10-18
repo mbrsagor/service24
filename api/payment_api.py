c53abf1b3d975721cae96402bb6a9935a2da3cc4
Sagor <mbrsagor@gmail.com>
2020-10-18 16:41:45 +0600

    Install Django filter

--- api/payment_api.py
+++ api/payment_api.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
+from django_filters.rest_framework import DjangoFilterBackend
 
 from service.models.payment import Payment
 
@@ -17,3 +18,5 @@ class PaymentViewSet(ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
     permission_classes = (IsAdminUser,)
+    filter_backends = [DjangoFilterBackend]
+    search_fields = ['name', 'id']
--- service24/settings.py
+++ service24/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+    'django_filters'
 ]
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"  # During development only
