7cb12577c8d3b60c41b1afb5ca86319d68d78659
Sagor <mbrsagor@gmail.com>
2020-11-03 15:17:49 +0600

    Service filter worked

--- service/filter/service_filter.py
+++ service/filter/service_filter.py
@@ -5,4 +5,4 @@ from service.models.service import Service
 class ServiceFilter(django_filters.FilterSet):
     class Meta:
         model = Service
-        fields = ['service_name', 'price', 'package_name']
+        fields = ['service_name', 'price', 'category', 'location']
