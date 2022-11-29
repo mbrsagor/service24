a3cbaa7d69fde62836c6b26c6399f8dd7dea5531
mbrsagor <mbrsagor@gmail.com>
2022-11-29 21:12:59 +0600

    Refactor: utils services and nginx refactor.

--- utils/mixin.py
+++ utils/mixin.py
@@ -7,39 +7,14 @@ def base_url():
     return uri
 
 
-# When issue will tranfered to contractor the body will call tranfer issue view.
+# When issue will transfer to contractor the body will call transfer issue view.
 def transfer_issue_body(title, budget, property_unit):
     msg = f"Dear contractor,\n\nwe have been transfer issue.\nIssue name: {title}\nIssue price: {budget}\nProperty unit:{property_unit}"
     return msg
 
 
-def transfer_issue_notify_admin(status, title, budget, property_unit):
-    """
-    Name: Transfer issue status.
-    Desc: When contractor will update their work status will be change from here.
-    :params `CONFIRM`, `PROGRESS`, `REJECTED`, `FINISHED`
-    """
-    msg = f"Dear Admin,\n{status}.\nIssue name: {title}\nIssue price: {budget}\nProperty unit:{property_unit}"
-    return msg
-
-
-# Assign issue crew/worker
-def assgin_issue_body(user, title, date, address):
-    msg = f"Dear {user}\n\nwe have assign a new issue please check the issuse details below.Issue name: {title}\nIssue date line: {date}\nCustomer address: {address}"
-    return msg
-
-
-# Worker completed issue mail body
-def completed_issue_body(user, title, date, address):
-    msg = f"Dear {user},\n\nYour issue has been successfully completed! please check the issuse details below.\nIssue name: {title}\nIssue date line: {date}\nCustomer address: {address}"
-    return msg
-
-
-
-
-# Date time convator
-def time_convartor(seconds):
-    min, sec = divmod(seconds, 60)
-    hour, min = divmod(min, 60)
-    return '%d:%02d:%02d' % (hour, min, sec)
-
+# Date time convert
+def time_convertor(seconds):
+    minute, sec = divmod(seconds, 60)
+    hour, minute = divmod(minute, 60)
+    return '%d:%02d:%02d' % (hour, minute, sec)
