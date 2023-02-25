50fe5c05d37ad9e0b14bb862850e961047a5b205
mbrsagor <mbrsagor@gmail.com>
2023-02-25 23:09:00 +0600

    Update: core app forms refactor.

--- core/forms/setting_form.py
+++ core/forms/setting_form.py
@@ -1,16 +1,16 @@
-from django.forms import ModelForm, TextInput, CheckboxInput, ImageField
+from django import forms
 from core.models.setting import Setting
 
 
-class SettingForm(ModelForm):
+class SettingForm(forms.ModelForm):
     class Meta:
         model = Setting
         fields = '__all__'
 
         widgets = {
-            'logo_upload': ImageField(attrs={'class': 'form-control'}),
-            'favicon_upload': ImageField(attrs={'class': 'form-control'}),
-            'site_name': TextInput(attrs={'class': 'form-control', 'id': 'site_name'}),
-            'is_active': CheckboxInput(attrs={'class': 'checkbox', 'id': 'is_active'}),
-            'copy_write_text': TextInput(attrs={'class': 'form-control', 'id': 'copy_write_text'})
+            'logo_upload': forms.ImageField(attrs={'class': 'form-control'}),
+            'favicon_upload': forms.ImageField(attrs={'class': 'form-control'}),
+            'site_name': forms.TextInput(attrs={'class': 'form-control', 'id': 'site_name'}),
+            'is_active': forms.CheckboxInput(attrs={'class': 'checkbox', 'id': 'is_active'}),
+            'copy_write_text': forms.TextInput(attrs={'class': 'form-control', 'id': 'copy_write_text'})
         }
