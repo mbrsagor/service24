d93fa20a656e7ba13352bc68950ab10a17a2f97f
mbrsagor <mbrsagor@gmail.com>
2023-03-30 16:21:33 +0600

    New: custom file uploaded function added.

--- utils/mixin.py
+++ utils/mixin.py
@@ -18,3 +18,12 @@ def time_convertor(seconds):
     minute, sec = divmod(seconds, 60)
     hour, minute = divmod(minute, 60)
     return '%d:%02d:%02d' % (hour, minute, sec)
+
+
+# Upload custom file path
+def upload_path_handler(instance, filename):
+    """
+    File or image media type file upload custom path
+    file = models.FileField(upload_to='upload_path_handler')
+    """
+    return "user_{id}/{file}".format(id=instance.user.id, file=filename)
