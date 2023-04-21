9aa96715525f8a608019750a642bf462fe59c636
mbrsagor <mbrsagor@gmail.com>
2023-04-21 23:29:44 +0600

    New: add host URL from setting.

--- utils/helper.py
+++ utils/helper.py
@@ -1,8 +1,8 @@
 import uuid
 from django.core.mail import send_mail
-
+from django.conf import settings
 
 def send_forgot_password_mail(email):
     token = str(uuid.uuid4())
     subject = 'Your forgot password link'
-    message = f"Hi, click on the link to forgot your password http://127.0.0.1:8000/forgot-password/{token}/"
+    message = f"Hi, click on the link to forgot your password {settings.HOST_URL}forgot-password/{token}/"
