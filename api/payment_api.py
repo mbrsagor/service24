7adad23eb1a0fb0037fb13f9a4a3613d1d4faf43
mbrsagor <mbrsagor@gmail.com>
2024-01-30 23:35:27 +0600

    New: added additional fields for engine PDF.

--- api/payment_api.py
+++ api/payment_api.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAdminUser
 from django_filters.rest_framework import DjangoFilterBackend
 
 from service.models.payment import Payment
+from utils.custom_pagination import CustomPagination
 
 
 class PaymentSerializer(ModelSerializer):
@@ -19,4 +20,5 @@ class PaymentViewSet(ModelViewSet):
     serializer_class = PaymentSerializer
     permission_classes = (IsAdminUser,)
     filter_backends = [DjangoFilterBackend]
+    pagination_class = CustomPagination
     search_fields = ['name',]
