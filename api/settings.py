64ac1f12303fdcf1ef1c7b99f7e84ce6851c449e
mbrsagor <mbrsagor@gmail.com>
2024-04-17 19:45:45 +0600

    New: Added comment settings, review, and service API endpoint

--- api/settings.py
+++ api/settings.py
@@ -12,6 +12,12 @@ class SettingSerializer(ModelSerializer):
 
 
 class ApplicationSettingViewSet(ModelViewSet):
+    """
+    Name: Application basic setting system.
+    Desc: API endpoint that allows applications to be viewed or edited.
+    Method: GET, POST, PUT, DELETE
+    URL: /api/v1/settings/
+    """
     queryset = Setting.objects.all()
     serializer_class = SettingSerializer
     permission_classes = [IsAuthenticated, ]
