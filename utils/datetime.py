6b6dc19b0549aefcd3ba8dc9325bd1693220759f
mbrsagor <mbrsagor@gmail.com>
2024-06-09 12:31:35 +0600

    New: Install new app.

---
+++ utils/datetime.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+np.datetime64('today')                         # numpy.datetime64('2023-09-17')
+np.datetime_as_string(np.datetime64('today'))  # '2023-09-17'
+str(np.datetime64('today'))
